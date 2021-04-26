82c729552bd1caf76530d70402c4c6dcd7a4e30d
EXDream <77859332+JeffreySheng3@users.noreply.github.com>
2021-04-26 17:00:48 -0400

    Add files via upload
    Removed comments and trailing undefined. Working submission.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var myArgs = process.argv.slice(2)
+var nums = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 
 function main(){
-	var nums = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 	for(i = 0; i < myArgs.length; i++){
 		var str = myArgs[i].toString();
 		for(j = 0; j < str.length; j++){
 			process.stdout.write(nums[Number(str[j])]);
-			// console.log(nums[Number(str[j])]);
 		}
 		if(i != myArgs.length-1){
 			process.stdout.write(",");
-			// console.log(",");
 		}
 	}
 }
+
+main()
