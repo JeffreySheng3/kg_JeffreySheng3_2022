6ccd2d3cddfbc29fe509b5790b8442484374c3fa
Jeffrey Sheng <jeffreysheng99@gmail.com>
2021-04-26 17:12:45 -0400

    Working submission.

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-var myArgs = process.argv.slice(2)
-var nums = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+var myArgs = process.argv.slice(2);
+var nums = [
+   "Zero",
+   "One",
+   "Two",
+   "Three",
+   "Four",
+   "Five",
+   "Six",
+   "Seven",
+   "Eight",
+   "Nine",
+];
 
-function main(){
-	for(i = 0; i < myArgs.length; i++){
-		var str = myArgs[i].toString();
-		for(j = 0; j < str.length; j++){
-			process.stdout.write(nums[Number(str[j])]);
-		}
-		if(i != myArgs.length-1){
-			process.stdout.write(",");
-		}
-	}
+function main() {
+   for (i = 0; i < myArgs.length; i++) {
+      var str = myArgs[i].toString();
+      for (j = 0; j < str.length; j++) {
+         process.stdout.write(nums[Number(str[j])]);
+      }
+      if (i != myArgs.length - 1) {
+         process.stdout.write(",");
+      }
+   }
 }
 
-main()
+main();
